662fb20af3a136d4497f3af487773c6d805fb2e1
MatheusMendes <50601403+mm490514@users.noreply.github.com>
2024-03-21 19:35:56 -0300

    feat: Script SQL

--- SQL.sql
+++ SQL.sql
@@ -1,25 +1,39 @@
-CREATE DATABASE `receitas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `clima` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-CREATE TABLE `igredientes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- clima.cidade definition
 
-CREATE TABLE `receitas` (
+CREATE TABLE `cidade` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
-  `modo_preparo` text,
-  `tempo` varchar(255) DEFAULT NULL,
-  `porcoes` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `receitas_igrediente` (
+-- clima.clima definition
+
+CREATE TABLE `clima` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `id_receita` int DEFAULT NULL,
-  `id_igrediente` int DEFAULT NULL,
-  `quantidade` float DEFAULT NULL,
-  `uni_med` varchar(255) DEFAULT NULL,
+  `id_cidade` varchar(255) DEFAULT NULL,
+  `temperatura` varchar(4) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `id_escala` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=773 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- clima.escala definition
+
+CREATE TABLE `escala` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `simbolo` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- clima.fornecedores definition
+
+CREATE TABLE `fornecedores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_arquivo` varchar(255) DEFAULT NULL,
+  `tipo_arquivo` varchar(4) DEFAULT NULL,
+  `nome_fornecedor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=498 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
